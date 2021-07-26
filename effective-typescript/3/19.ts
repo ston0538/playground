b4b3c89bc376c22bc24f6c625fe5fea25d806004
KHK <ston0538@gmail.com>
2021-07-26 22:58:25 +0900

    feat: 이펙티브 타입스크립트 3장, 아이템19

--- effective-typescript/3/19.ts
+++ effective-typescript/3/19.ts
@@ -37,4 +37,51 @@ function square(nums: number[]) {
 }
 const squares = square([1, 2, 3, 4])
 
+const axis1: string = "x"
+const axis2 = "y"
 
+interface Product {
+  id: number;
+  name: string;
+  price: number
+}
+
+function logProduct(product: Product) {
+  const id: number = product.id
+  const name: string = product.name
+  const price: number = product.price
+  console.log(id, name, price)
+}
+function logProduct2(product: Product) {
+  const { id, name, price } = product
+  console.log(id, name, price)
+}
+const product = {
+  id: 1,
+  name: "A.P.C",
+  price: 1200,
+}
+const product2 = {
+  id: "hello",
+  name: "A.P.C",
+  price: 1200,
+}
+logProduct(product)
+logProduct2(product2)
+
+function parseNumber(str: string, base = 10) {
+  ///
+}
+
+
+
+const elmo: Product = {
+  id: '123412 12121',
+  name: "HK",
+  price: 28.99
+}
+
+interface Vector2D { x: number, y: number }
+function add(a: Vector2D, b: Vector2D) {
+  return { x: a.x + b.x, y: a.y + b.y }
+}
