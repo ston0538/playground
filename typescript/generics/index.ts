60f4afd61dff67cf20e83d462ddc450a75b1173e
KHK <ston0538@gmail.com>
2021-07-29 11:32:28 +0900

    feat: 제너릭 추가

--- typescript/generics/index.ts
+++ typescript/generics/index.ts
@@ -9,3 +9,12 @@ function identity2<T>(arg: T[]): T[] {
   return arg
 }
 identity2(3)
+
+
+let myObj = { a: 1, b: 2, c: 3 }
+
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key]
+}
+
+console.log(getProperty(myObj, "a"))
