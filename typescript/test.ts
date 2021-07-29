07166e356f8cb06a1f6ff5a15ffadad773949154
KHK <ston0538@gmail.com>
2021-07-29 11:40:00 +0900

    feat: git patch test

--- typescript/test.ts
+++ typescript/test.ts
@@ -2,11 +2,11 @@ function add(a, b) {
   return a + b
 }
 function sub(a, b) {
-  return a + b
+  return a - b
 }
 function multiply(a, b) {
-  return a + b
+  return a * b
 }
 function divide(a, b) {
-  return a + b
+  return a / b
 }
